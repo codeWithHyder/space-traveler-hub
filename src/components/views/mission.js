e180f2c6d44e5dc09648bc5d612b6e7309c7b65e
Inayat-khan <khanetmoi@gmail.com>
2023-04-19 12:57:41 +0000

    mission setup

--- src/components/views/mission.js
+++ src/components/views/mission.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Mission() {
   return (
     <div>
-      <h1>Mission will be implemented soon!</h1>
+      <h1>Mission will be implemented soon !</h1>
     </div>
   );
 }
