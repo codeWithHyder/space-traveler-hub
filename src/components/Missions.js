302226387776c90e73ad806de74dfecf36e3ee78
Inayat-khan <khanetmoi@gmail.com>
2023-04-20 05:20:12 +0000

    linters errors

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMissions } from '../redux/missions/Missions';
 import './styles/Missions.css';
-import Mission from './Mission';
+import Mission from './mission';
 import Loader from './Loader';
 import 'bootstrap/dist/css/bootstrap.css';
 
