b5ac3d2ccb2cf25e1fc983bbf1d5658208c7d235
Hyder <hyderali400@gmail.com>
2023-04-20 10:09:22 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import './styles/Navbar.css';
 import logo from './logo.png';
-
 const Navbar = () => (
   <nav className="nav-bar">
     <div className="logo">
